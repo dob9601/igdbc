0e55ac781fb06afa3fe98ccba6d156823d418bfb
Daniel O'Brien <dob9601@gmail.com>
2022-11-11 22:07:05 +0000

    Clippy

--- src/models/query.rs
+++ src/models/query.rs
@@ -1,7 +1,7 @@
 use chrono::Utc;
 use sea_orm::{prelude::*, Set};
 
-#[derive(Clone, Debug, PartialEq, DeriveEntityModel)]
+#[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel)]
 #[sea_orm(table_name = "queries")]
 pub struct Model {
     #[sea_orm(primary_key, auto_increment = false)]
